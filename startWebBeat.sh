9b4f90aab276e775fee13b0b2d943e7a5ededf6c
Jan Holomek <jahhoo@gmail.com>
2023-07-27 01:09:29 +0200

    Skript pro spuštění WebBeatu

---
+++ startWebBeat.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+mkdir -p logs
+date_now=$(date "+%F_%H-%M-%S")
+url=''
+page=1
+i=0;
+
+
+while [ $i -lt $page ]; do 
+
+	json=$(curl -s 'http://10.3.0.24/api/urlFeeder/?page='$i);
+
+	page=$(echo $json | python3 -c "import sys, json; print(json.load(sys.stdin)['sum']['pages'])")
+	url=$(echo $json | python3 -c "import sys, json; print(json.load(sys.stdin)['data'])")
+
+	url=$(echo "${url//"'"/}")
+	url=$(echo "${url//","/}")
+	url=$(echo "${url//"["/}")
+	url=$(echo "${url//"]"/}")
+	
+	python3 /opt/WebBeat/WebBEAT.py -p 5 --no-whois -e http://10.3.0.24/api/v2/ -s "$url" >> logs/log$date_now.log
+
+	i+=1
+done
