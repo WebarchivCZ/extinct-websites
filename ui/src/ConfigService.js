37e7a2ddf087a2fe8b4fb54a1e10a15fe2616a59
Jan Holomek <jahhoo@gmail.com>
2024-08-28 14:55:00 +0200

    Odstranění WebBeat

--- ui/src/ConfigService.js
+++ ui/src/ConfigService.js
@@ -1,7 +1,8 @@
 import { writable } from 'svelte/store';
 
-
-export const api = writable("/api/v2/");
-export const apiCheck = writable("/autocheck");
+export const api = writable("http://10.3.0.24/api/v2/");
+export const apiCheck = writable("http://10.3.0.24/autocheck");
+export const apiFeeder = writable("http://10.3.0.24/api/urlFeeder/");
 //export const db = writable("webarchiv");
-export const db = writable("");
+//export const db = writable("test2");
+export const db = writable("test2");
